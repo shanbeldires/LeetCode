1592d62ae243e6fa5903b5116776a091684978e6
shanbel <shanbeldires7@gmail.com>
2026-03-06 20:56:17 +0300

    initial commit

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+function twoSum(nums,target){
+    let map={};
+
+    for(let i=0;i<nums.length;i++){
+        
+        let complement=target-nums[i];
+
+        if(map[complement!==undefined]){
+            return [map[complement],i];
+        }
+
+        map[nums[i]]=i;
+
+    }
+}
+twoSum([2,7,11,15],9)
